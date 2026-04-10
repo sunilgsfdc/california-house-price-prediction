5ba3e0e54d536c27fe98b7de94d9acf2f273174b
unknown <sunilgsfdc@gmail.com>
2026-04-10 21:51:01 +0530

    Add changes to yml

--- app.py
+++ app.py
@@ -1,32 +1,21 @@
 import uvicorn
 from fastapi import FastAPI
-import  joblib
-from pydantic import BaseModel
-from typing import List
+import joblib
 import pandas as pd
-class PredictionInput(BaseModel):
-    features : List[float]
 
 app = FastAPI()
 
 model = joblib.load("./artifacts/model.pkl")
 
 @app.get("/")
-def test():
-    return {"message":"ML API Is running"}
+def test_api():
+    return {"message":"Test API"}
 
-'''
-{
-"features" :[5.1,3.5,1.4,0.2]
-}
-'''
 @app.post("/predict")
-def predict(data:PredictionInput):
-    df = pd.DataFrame([data.features])
+def predict(data:dict):
+    df = pd.DataFrame[data["features"]]
     prediction = model.predict(df)
-    return {"prediction":int(prediction)}
+    return {"prediction":float(prediction[0])}
 
-
-
-if __name__ == '__main__':
-    uvicorn.run("app:app", host="127.0.0.1", port=8001, reload=True)
+if __name__ = '__main__':
+    uvicorn.run("app:app", host="0.0.0.0", port=8000)
--- train.py
+++ train.py
@@ -1,41 +1,27 @@
-import pickle
 
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 import os
+import pandas as pd
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 import joblib
-from pathlib import Path
-import json
-
-
-
 def main():
-    iris = load_iris()
-    x,y = iris.data, iris.target
-    print(x)
-    print(y)
-    print(iris.target_names)
+    data = fetch_california_housing()
+    X = pd.DataFrame(data.data, columns=data.feature_names)
+    y = data.target
+    # print(X.head())
+    # print(y)
+    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
 
-    # model is a mathematical equation
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 42)
+    # Train
+    model = LinearRegression()
+    model.fit(X_train,y_train)
 
-    model = LogisticRegression(max_iter=200) # algorithm
-    model.fit(X_train, y_train) # training the model
-
-    if not Path('artifacts').exists():
-        os.mkdir('artifacts')
-
-    model_path = os.path.join('artifacts', 'model.pkl')
+    # Save the model
+    os.mkdir("artifacts")
+    model_path = os.path.join("artifacts", "model.pkl")
     joblib.dump(model, model_path)
 
-    acc = model.score(X_test, y_test) # checking for accuracy of the data
-    metrics = {'accuracy': float(acc)}
-
-    with open(os.path.join('artifacts', 'metrics.json'), 'w') as f:
-        json.dump(metrics, f)
-
-    print(acc)
 
 if __name__ == '__main__':
     main()
