507d6171da06ac862e7a58c5c9ea5d7a2065768a
unknown <sunilgsfdc@gmail.com>
2026-04-13 19:57:03 +0530

    Test Model

--- train.py
+++ train.py
@@ -1,4 +1,3 @@
-
 import os
 import pandas as pd
 from sklearn.datasets import fetch_california_housing
@@ -6,11 +5,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import joblib
 def main():
-    data = fetch_california_housing()
-    X = pd.DataFrame(data.data, columns=data.feature_names)
-    y = data.target
-    # print(X.head())
-    # print(y)
+    df = pd.read_csv("./data/housing.csv")
+    print(df.head())
+    X = df.drop("target", axis=1)
+    y = df["target"]
     X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
 
     # Train
@@ -21,6 +19,7 @@ def main():
     os.mkdir("artifacts")
     model_path = os.path.join("artifacts", "model.pkl")
     joblib.dump(model, model_path)
+
 
 
 if __name__ == '__main__':
